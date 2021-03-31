36bac1aabe125a0cb8712124fcd549fb62380773
Luis Felipe Ceballos Caicedo <ceballos-65@hotmail.com>
2021-03-30 23:02:54 -0500

    create product-component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
+import React from 'react';
 import './index.css';
+// Data Product
+import data from './data.json';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <nav>
-          <a href="/">ShoppingCart</a>
-        </nav>
-      </header>
-      <main>Product List</main>
-      <footer>All right is reserved.</footer>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = { 
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  
+  render() { 
+    return (
+      <div className="grid-container">
+        <header>
+          <nav>
+            <a href="/">ShoppingCart</a>
+          </nav>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">
+              Cart Items
+            </div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }  
 }
 
 export default App;
